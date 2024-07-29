efb2eaef001471087a89d4616a98ae3732f433c9
Rakesh Prashant Mal <malrakesh172@gmail.com>
2024-07-29 21:58:00 +0530

    initial commit

---
+++ StringOperation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringOperation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Console Application to perform various string operations.
+
+            Console.WriteLine("*** String Operations ***");
+
+            string str1 = "Rakesh", str2 = " Mal";
+            Console.WriteLine("\nString1 - " + str1);
+            Console.WriteLine("\nString2 - " + str2);
+
+            Console.WriteLine("\n--------------------------");
+
+            //Length
+            Console.WriteLine("\nLength of String1 - " + str1.Length);
+
+            //Concat()
+            Console.WriteLine("\nConcat() - " + String.Concat(str1, str2));
+
+            //Trim()
+            Console.WriteLine("\nTrim() - " + str1.Trim('h'));
+
+            //ToUpper()
+            Console.WriteLine("\nToUpper() - " + str1.ToUpper());
+
+            //ToLower()
+            Console.WriteLine("\nToLower() - " + str2.ToLower());
+
+            //Substring()
+            Console.WriteLine("\nSubstring() - " + str1.Substring(0, 2));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SumOfProd.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfProd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // An application that obtains four int values from the user and displays the sum product
+
+            int no1, no2, no3, no4, product;
+
+            Console.WriteLine("--- Sum Of Numbers ---");
+
+            Console.Write("\nEnter Number 1 : ");
+            no1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nEnter Number 2 : ");
+            no2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nEnter Number 3 : ");
+            no3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nEnter Number 4 : ");
+            no4 = Convert.ToInt32(Console.ReadLine());
+
+            product = no1 * no2 * no3 * no4;
+
+            Console.Write("\nProduct is " + product);
+
+            Console.ReadKey();
+        }
+    }
+}
